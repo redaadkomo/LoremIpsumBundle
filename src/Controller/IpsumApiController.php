2710fabfa133be024a00f51734ebbc48dcf20dc7
Reda Benzekri <reda@adkomo.com>
2020-06-25 00:14:55 +0200

    add

--- src/Controller/IpsumApiController.php
+++ src/Controller/IpsumApiController.php
@@ -1,23 +1,28 @@
 <?php
-
+namespace KnpU\LoremIpsumBundle\Controller;
+use KnpU\LoremIpsumBundle\Event\FilterApiResponseEvent;
+use KnpU\LoremIpsumBundle\KnpUIpsum;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-class IpsumApiController extends AbstractController {
-
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+class IpsumApiController extends AbstractController
+{
     private $knpUIpsum;
     private $eventDispatcher;
-
-    public function __construct(KnpUIpsum $knpUIpsum, EventDispatcherInterface $eventDispatcher) {
+    public function __construct(KnpUIpsum $knpUIpsum, EventDispatcherInterface $eventDispatcher = null)
+    {
         $this->knpUIpsum = $knpUIpsum;
         $this->eventDispatcher = $eventDispatcher;
     }
-
-    public function index() {
-
+    public function index()
+    {
+        $data = [
+            'paragraphs' => $this->knpUIpsum->getParagraphs(),
+            'sentences' => $this->knpUIpsum->getSentences(),
+        ];
         $event = new FilterApiResponseEvent($data);
-        $this->eventDispatcher->dispatch('knpu_lorem_ipsum.filter_api', $event);
-
+        if ($this->eventDispatcher) {
+            $this->eventDispatcher->dispatch('knpu_lorem_ipsum.filter_api', $event);
+        }
         return $this->json($event->getData());
     }
-
 }
