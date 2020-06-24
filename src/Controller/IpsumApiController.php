1d86dff5b4261a63f5dc2592941a4a274fcd0b78
Reda Benzekri <reda@adkomo.com>
2020-06-25 01:50:08 +0200

    add

--- src/Controller/IpsumApiController.php
+++ src/Controller/IpsumApiController.php
@@ -1,28 +1,10 @@
 <?php
 namespace KnpU\LoremIpsumBundle\Controller;
-use KnpU\LoremIpsumBundle\Event\FilterApiResponseEvent;
-use KnpU\LoremIpsumBundle\KnpUIpsum;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-class IpsumApiController extends AbstractController
+
+class IpsumApiController
 {
-    private $knpUIpsum;
-    private $eventDispatcher;
-    public function __construct(KnpUIpsum $knpUIpsum, EventDispatcherInterface $eventDispatcher = null)
-    {
-        $this->knpUIpsum = $knpUIpsum;
-        $this->eventDispatcher = $eventDispatcher;
-    }
     public function index()
     {
-        $data = [
-            'paragraphs' => $this->knpUIpsum->getParagraphs(),
-            'sentences' => $this->knpUIpsum->getSentences(),
-        ];
-        $event = new FilterApiResponseEvent($data);
-        if ($this->eventDispatcher) {
-            $this->eventDispatcher->dispatch('knpu_lorem_ipsum.filter_api', $event);
-        }
-        return $this->json($event->getData());
+        dd("5445");
     }
 }
--- src/DependencyInjection/KnpULoremIpsumExtension.php
+++ src/DependencyInjection/KnpULoremIpsumExtension.php
@@ -10,11 +10,7 @@ class KnpULoremIpsumExtension extends Extension
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
-        $configuration = $this->getConfiguration($configs, $container);
-        $config = $this->processConfiguration($configuration, $configs);
-        $definition = $container->getDefinition('knpu_lorem_ipsum.knpu_ipsum');
-        $definition->setArgument(0, $config['unicorns_are_real']);
-        $definition->setArgument(1, $config['min_sunshine']);
+       
     }
     public function getAlias()
     {
